75ab3e035fe715fd96929606d4d2d7ec3ae4b928
mehmetcetin43 <mehmet_cetin_43@hotmail.com>
2021-12-20 17:12:28 +0300

    Add project files.

---
+++ Blog.Entities/Concrete/Category.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Entity.Concrete
+{
+    public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public ICollection<Article> Articles { get; set; }
+
+    }
+}
---
+++ Blog.Entities/Concrete/User.cs
@@ -0,0 +1,13 @@
+﻿using Blog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Entity.Concrete
+{
+    public class User:IEntity
+    {
+    }
+}
