9b9e60bbd8576279aff8e8f053185f9853c492e0
salih atici <salihatici50@gmail.com>
2022-01-13 18:22:48 +0300

    EntityRepository oluşturuldu

---
+++ Blog.Shared/Data/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Blog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Shared.Data.Abstract
+{
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter);
+        IQueryable<T> QueryGetList(Expression<Func<T, bool>> filter = null);
+
+    }
+}
