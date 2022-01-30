b8585f5214d6cf972753fd7c30a699b5491bbc0c
slhatc50 <slhatc50@outlook.com>
2022-01-30 20:56:06 +0300

    Result ve dataresult yapısı oluşturuldu

---
+++ Blog.Shared/Utilities/Results/ComplexTypes/ResultStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Shared.Utilities.Results.ComplexTypes
+{
+    public enum ResultStatus
+    {
+        Success=0,
+        Failed=1,
+        Warning=2,
+        Information=3,
+
+    }
+}
